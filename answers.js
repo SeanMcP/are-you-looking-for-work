5b04235a7b6f524a03e43a04a73f2690606f469f
Sean McPherson <seanmcp25@gmail.com>
2018-11-09 09:08:54 -0500

    Add basics

---
+++ answers.js
@@ -0,0 +1,7 @@
+var areYouLookingForWork = false;
+
+var titles = ['Frontend Developer', 'UI/UX Developer'];
+
+var technologies = ['JavaScript', 'React', 'Python'];
+
+var locations = ['Pittsburgh, PA', 'Philadelphia, PA', 'New York, NY', 'Washington, DC'];
