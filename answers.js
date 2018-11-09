54999dfc5407b6dcd73011f8faf6c1cc1ea89236
Sean McPherson <seanmcp25@gmail.com>
2018-11-09 16:39:21 -0500

    Add comments

--- answers.js
+++ answers.js
@@ -1,7 +1,10 @@
+// I am happily (and gainfully) employed.
 var amILookingForWork = false;
 
 var titles = ['Frontend Developer', 'UI/UX Developer'];
 
 var technologies = ['JavaScript', 'React'];
 
+// I am content living in Atlanta now, but my long-term
+// future lies closer to family in Pennsylvania.
 var locations = ['Pittsburgh, PA', 'Philadelphia, PA', 'New York, NY', 'Washington, DC'];
